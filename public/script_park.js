376e285d101683f1ad48833736246b4797abbc23
tomoakari <realize.arts@gmail.com>
2020-07-28 00:06:23 +0900

    修正

--- public/script_park.js
+++ public/script_park.js
@@ -6,7 +6,7 @@ function attachVideo(id, stream) {
   playVideo(video, stream);
   video.volume = 1.0;
 
-  $("#remote_video_" + id).wrap('<div class="col-3 col-12-small" id="video_container_' + id + '"/>');
+  $("#remote_video_" + id).wrap('<div class="col-1 col-12-small" id="video_container_' + id + '"/>');
   $("#remote_video_" + id).after(
     '<p class="membername" id="user_name_' + id + '">　</p>'
   );
@@ -14,5 +14,5 @@ function attachVideo(id, stream) {
 
 // ダミーのビデオを追加する
 function addBlankVideoElement(){
-  $('#container').append('<div class="col-3 col-12-small blankVideo" ><img src="/assets/images/dummy.png" class="dummyvideo"/></div>');
+  $('#container').append('<div class="col-1 col-12-small blankVideo" ><img src="/assets/images/dummy.png" class="dummyvideo"/></div>');
 }
