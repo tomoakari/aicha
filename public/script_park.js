16b52f24741d6946f572940715619a7ea8b29e8a
tomoakari <realize.arts@gmail.com>
2020-07-28 00:05:09 +0900

    ビデオ表示サイズの出しわけ

---
+++ public/script_park.js
@@ -0,0 +1,18 @@
+パーク用オーバーライド
+
+
+function attachVideo(id, stream) {
+  let video = addRemoteVideoElement(id);
+  playVideo(video, stream);
+  video.volume = 1.0;
+
+  $("#remote_video_" + id).wrap('<div class="col-3 col-12-small" id="video_container_' + id + '"/>');
+  $("#remote_video_" + id).after(
+    '<p class="membername" id="user_name_' + id + '">　</p>'
+  );
+}
+
+// ダミーのビデオを追加する
+function addBlankVideoElement(){
+  $('#container').append('<div class="col-3 col-12-small blankVideo" ><img src="/assets/images/dummy.png" class="dummyvideo"/></div>');
+}
