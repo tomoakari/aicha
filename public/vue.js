2469bcd102db090dfdbfd3564fd6823e04f76817
tomoakari <realize.arts@gmail.com>
2020-09-08 12:41:54 +0900

    たぶんきた

--- public/vue.js
+++ public/vue.js
@@ -205,7 +205,38 @@ var roomVue = new Vue({
       });
     },
     jumpRoom: function (event) {
-      alert(event.target.innerText);
+      const param = {
+        table_name: event.target.innerText,
+        user_name: $("#user_name").val(),
+      };
+      execPost("", param);
+    },
+    /**
+     * データをPOSTする
+     * @param String アクション
+     * @param Object POSTデータ連想配列
+     * 記述元Webページ http://fujiiyuuki.blogspot.jp/2010/09/formjspost.html
+     * サンプルコード
+     * <a onclick="execPost('/hoge', {'fuga':'fuga_val', 'piyo':'piyo_val'});return false;" href="#">POST送信</a>
+     */
+    execPost: function (action, data) {
+      var form = document.createElement("form");
+      form.setAttribute("action", action);
+      form.setAttribute("method", "post");
+      form.style.display = "none";
+      document.body.appendChild(form);
+      // パラメタの設定
+      if (data !== undefined) {
+        for (var paramName in data) {
+          var input = document.createElement("input");
+          input.setAttribute("type", "hidden");
+          input.setAttribute("name", paramName);
+          input.setAttribute("value", data[paramName]);
+          form.appendChild(input);
+        }
+      }
+      // submit
+      form.submit();
     },
   },
 });
