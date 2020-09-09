38a17acae927fb49fc12a36dd8b421edb259c6a7
tomoakari <realize.arts@gmail.com>
2020-09-09 15:44:18 +0900

    ルート変更

--- server.js
+++ server.js
@@ -61,11 +61,7 @@ app.get("/createroom", (request, response) => {
   const room_name = request.query.name;
   console.log("request:" + category_id + " / " + room_name);
   const resultData = chackAndCreateRoom(category_id, room_name);
-  response.json({
-    statusText: "てすとだぞ",
-    ok: true,
-    room_name: "てすとるーむ",
-  });
+  response.json(resultData);
 });
 
 // 秘密の管理ページ
@@ -348,6 +344,8 @@ function chackAndCreateRoom(category_id, room_name) {
           };
           createRoom(data);
           const result = {
+            statusText: "OK",
+            ok: true,
             room_name: room_name,
           };
           return result;
